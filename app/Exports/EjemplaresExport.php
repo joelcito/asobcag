17085f6d83d7481cdc72c84254fcb421eae45c82
DanBlyz <benjo123gonza@gmail.com>
2025-02-27 18:13:26 -0400

    Cmabios en la exportacion de excel de ejemplares de criadero

--- app/Exports/EjemplaresExport.php
+++ app/Exports/EjemplaresExport.php
@@ -4,18 +4,17 @@ namespace App\Exports;
 
 use App\Models\Criadero;
 use App\Models\Ejemplar;
-use App\Models\Biometria;
-use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\FromArray;
 use Maatwebsite\Excel\Concerns\WithStyles;
 use Maatwebsite\Excel\Concerns\WithHeadings;
-
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithMergedCells;
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
 class EjemplaresExport implements FromArray, WithHeadings, WithStyles, ShouldAutoSize
 {
     protected $criadero_id;
+    protected $mergeCells = [];
 
     public function __construct($criadero_id)
     {
@@ -24,7 +23,6 @@ class EjemplaresExport implements FromArray, WithHeadings, WithStyles, ShouldAut
 
     public function array(): array
     {
-        // Obtener datos del criadero
         $criadero = Criadero::with(['propietario', 'tecnico', 'pastor'])
                             ->find($this->criadero_id);
 
@@ -32,28 +30,32 @@ class EjemplaresExport implements FromArray, WithHeadings, WithStyles, ShouldAut
             return [['Error' => 'Criadero no encontrado']];
         }
 
-        // Obtener los ejemplares asociados
         $ejemplares = Ejemplar::where('criadero_id', $this->criadero_id)
-            ->with(['color', 'fenotipo', 'biometrias', 'morfologicos'])
+            ->with(['color', 'fenotipo', 'raza', 'biometrias', 'morfologicos'])
             ->get();
 
-        // Construir los datos en un array
         $data = [];
 
-        // Agregar datos del criadero en una fila independiente
+        // Agregar información del criadero
         $data[] = ['CRIADERO:', $criadero->nombre ?? 'N/A'];
         $data[] = ['Propietario:', $criadero->propietario->name ?? 'N/A'];
-        $data[] = ['tecnico:', $criadero->tecnico->name ?? 'N/A'];
+        $data[] = ['Técnico:', $criadero->tecnico->name ?? 'N/A'];
         $data[] = ['Pastor:', $criadero->pastor->name ?? 'N/A'];
-
-        // Fila vacía como separación
         $data[] = [];
 
-        // Agregar encabezados de los ejemplares
-        $data[] = ['ID', 'Nombre', 'Sexo', 'Nro. de Registro', 'Micropchip', 'Arete', 'Fecha de Nacimiento', 'Fecha de Registro', 'Color', 'Fenotipo', 'Raza', 'Biometrias', 'Morfologicos'];
+        // Encabezados de la tabla
+        $headers = ['ID', 'Nombre', 'Sexo', 'Nro. Registro', 'Microchip', 'Arete', 'Fecha Nac.', 'Fecha Reg.', 'Color', 'Fenotipo', 'Raza', 'Biometrías', 'Morfología'];
+        $data[] = $headers;
 
-        // Agregar los ejemplares
+        // Registro de datos
+        $startRow = 7; // Comienza después de los encabezados
+
         foreach ($ejemplares as $ejemplar) {
+            // Determinar la cantidad de filas a fusionar
+            $maxFilas = max($ejemplar->biometrias->count(), $ejemplar->morfologicos->count());
+            $maxFilas = $maxFilas > 0 ? $maxFilas : 1; // Al menos una fila
+
+            // Agregar datos del ejemplar en la primera fila
             $data[] = [
                 $ejemplar->id,
                 $ejemplar->nombre,
@@ -66,9 +68,33 @@ class EjemplaresExport implements FromArray, WithHeadings, WithStyles, ShouldAut
                 $ejemplar->color->nombre ?? 'N/A',
                 $ejemplar->fenotipo->nombre ?? 'N/A',
                 $ejemplar->raza->nombre ?? 'N/A',
-                $this->stringBiometrias($ejemplar->biometrias),
-                $this->stringMorfologicos($ejemplar->morfologicos),
+                '', // Espacio reservado para Biometrías
+                ''  // Espacio reservado para Morfología
             ];
+
+            // Guardar las celdas a fusionar para los datos del ejemplar (columnas 1-11)
+            $this->mergeCells[] = ["A$startRow:J" . ($startRow + $maxFilas - 1)];
+
+            // Agregar las biometrias y morfologías alineadas
+            for ($i = 0; $i < $maxFilas; $i++) {
+                $biometriaTexto = isset($ejemplar->biometrias[$i]) ? 
+                    "📌 #".($i + 1)." - Motivo: {$ejemplar->biometrias[$i]->motivo}, Fecha: {$ejemplar->biometrias[$i]->fecha}, Peso: {$ejemplar->biometrias[$i]->peso} kg" 
+                    : '';
+
+                $morfologicoTexto = isset($ejemplar->morfologicos[$i]) ? 
+                    "📌 #".($i + 1)." - Motivo: {$ejemplar->morfologicos[$i]->motivo}, Evaluación: {$ejemplar->morfologicos[$i]->fecha_evaluacion}, Oreja: {$ejemplar->morfologicos[$i]->oreja}" 
+                    : '';
+
+                if ($i > 0) {
+                    $data[] = ['', '', '', '', '', '', '', '', '', '', '', $biometriaTexto, $morfologicoTexto];
+                } else {
+                    $data[count($data) - 1][11] = $biometriaTexto;
+                    $data[count($data) - 1][12] = $morfologicoTexto;
+                }
+            }
+
+            // Avanzar la fila de inicio para el siguiente ejemplar
+            $startRow += $maxFilas;
         }
 
         return $data;
@@ -79,83 +105,20 @@ class EjemplaresExport implements FromArray, WithHeadings, WithStyles, ShouldAut
         return [];
     }
 
-    // 👉 Estilos para encabezados en negrita
     public function styles(Worksheet $sheet)
     {
         return [
-            // Aplicar negrita a las filas del criadero (1 a 4) y los encabezados de ejemplares (fila 6)
-            1 => ['font' => ['bold' => true]],
+            1 => ['font' => ['bold' => true]], // Negrita en encabezado de criadero
             2 => ['font' => ['bold' => true]],
             3 => ['font' => ['bold' => true]],
             4 => ['font' => ['bold' => true]],
-            5 => ['font' => ['bold' => true]],
+            5 => ['font' => ['bold' => true]], // Negrita en encabezados de tabla
         ];
     }
 
-    /* FUNCIONES EXTRAS PARA EL ARRAY */
-    public function stringBiometrias(Collection $biometrias){
-        if($biometrias->isNotEmpty()){
-            $cadena = '';
-            $indice = 1;
-            foreach($biometrias as $b){
-                $cadena = $cadena.'.- '.$indice.
-                ' Motivo: '.$b->motivo.
-                ' Fecha: '.$b->fecha.
-                ' Peso: '.$b->peso.
-                ' Altura Cruz: '.$b->altura_cruz.
-                ' Altura Grupa: '.$b->altura_grupa.
-                ' Altura Cabeza: '.$b->altura_cabeza.
-                ' Ancho Pecho: '.$b->ancho_pecho.
-                ' Ancho Isquiones: '.$b->ancho_isquiones.
-                ' Perimetro Toraxico: '.$b->perimetro_toraxico.
-                ' Perimetro Abdominal: '.$b->perimetro_abdominal.
-                ' Largo Cuello: '.$b->largo_cuello.
-                ' Cuello Perimetro Sup: '.$b->cuello_perimetro_sup.
-                ' Cuello Perimetro Inf: '.$b->cuello_perimetro_inf.
-                ' Largo Oreja: '.$b->largo_oreja.
-                ' Largo Cola: '.$b->largo_cola.
-                ' Largo Cuerpo: '.$b->largo_cuerpo.
-                ' Diametro Cania Ant: '.$b->diametro_cania_ant.
-                ' Diametro Cania Post: '.$b->diametro_cania_post.
-                "\n";
-                $indice++;
-            }
-            return $cadena;
-        }
-
-        return 'N/A';
+    public function mergedCells()
+    {
+        return $this->mergeCells;
     }
-
-    public function stringMorfologicos(Collection $morfologicos){
-        if($morfologicos->isNotEmpty()){
-            $cadena = '';
-            $indice = 1;
-            foreach($morfologicos as $m){
-                $cadena = $cadena.'.- '.$indice.
-                ' Motivo: '.$m->motivo.
-                ' Fecha Evaluacion: '.$m->fecha_evaluacion.
-                ' Oreja: '.$m->oreja.
-                ' Cuello: '.$m->cuello.
-                ' Cabeza: '.$m->cabeza.
-                ' Alzada: '.$m->alzada.
-                ' Largo Cuerpo: '.$m->largo_cuerpo.
-                ' Amplitud Pecho: '.$m->amplitud_pecho.
-                ' Fortaleza: '.$m->fortaleza.
-                ' Balance: '.$m->balance.
-                ' Canias: '.$m->canias.
-                ' Copete: '.$m->copete.
-                ' Linea Superior: '.$m->linea_superior.
-                ' Grupa: '.$m->grupa.
-                ' Densidad: '.$m->densidad.
-                ' Rizo: '.$m->rizo.
-                ' Calce: '.$m->calce.
-                "\n";
-                $indice++;
-            }
-            return $cadena;
-        }
-
-        return 'N/A';
-    }
-
 }
+
