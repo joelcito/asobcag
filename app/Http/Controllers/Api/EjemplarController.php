5e1afeceee25fc5eb38a12e4462de9c7b3b92307
joelcito <jjjoelcito123@gmail.com>
2025-03-10 01:16:36 -0400

    se modifoic el token

--- app/Http/Controllers/Api/EjemplarController.php
+++ app/Http/Controllers/Api/EjemplarController.php
@@ -35,6 +35,30 @@ class EjemplarController extends Controller
             //                         ->join('criaderos', 'ejemplares.criadero_id', '=', 'criaderos.id')
             //                         ->where('criaderos.propietario_id', $usuario->id)->get();
 
+            // $ejemplares = Ejemplar::select(
+            //     'ejemplares.id',
+            //     'ejemplares.padre_id',
+            //     'ejemplares.madre_id',
+            //     'ejemplares.fenotipo_id',
+            //     'ejemplares.color_id',
+            //     'ejemplares.nombre',
+            //     'ejemplares.sexo',
+            //     'ejemplares.fecha_nacimiento',
+            //     'ejemplares.numero_registro',
+            //     'ejemplares.microchip',
+            //     'ejemplares.arete',
+            //     'ejemplares.tipo'
+            // )
+            // ->join('criaderos', 'ejemplares.criadero_id', '=', 'criaderos.id')
+            // ->leftJoin('ejemplar_imagenes', 'ejemplares.id', '=', 'ejemplar_imagenes.ejemplar_id') // Unir las imágenes
+            // ->where('criaderos.propietario_id', $usuario->id)
+            // ->get()
+            // ->map(function ($ejemplar) {
+            //     $ejemplar->imagenes = EjemplarImagen::where('ejemplar_id', $ejemplar->id)
+            //         ->pluck('ruta');
+            //     return asset().$ejemplar;
+            // });
+
             $ejemplares = Ejemplar::select(
                 'ejemplares.id',
                 'ejemplares.padre_id',
@@ -54,10 +78,15 @@ class EjemplarController extends Controller
             ->where('criaderos.propietario_id', $usuario->id)
             ->get()
             ->map(function ($ejemplar) {
+                // Obtener las imágenes y devolver la URL completa con asset()
                 $ejemplar->imagenes = EjemplarImagen::where('ejemplar_id', $ejemplar->id)
-                    ->pluck('ruta');
+                    ->pluck('ruta') // Obtener solo el campo 'ruta'
+                    ->map(function ($ruta) {
+                        return asset($ruta); // Concatenar el dominio y la ruta de la imagen
+                    });
                 return $ejemplar;
             });
+
 
 
             return response()->json([
