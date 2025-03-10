89886cac60d78607cc8d9fb9167842c17f9df739
joelcito <jjjoelcito123@gmail.com>
2025-03-10 00:02:02 -0400

    se modifoic el token

--- app/Http/Controllers/Api/EjemplarController.php
+++ app/Http/Controllers/Api/EjemplarController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Tymon\JWTAuth\Facades\JWTAuth;
 use App\Models\Ejemplar;
+use App\Models\EjemplarImagen;
 
 class EjemplarController extends Controller
 {
@@ -16,23 +17,48 @@ class EjemplarController extends Controller
             if (!$usuario)
                 return response()->json(['error' => 'Usuario no encontrado'], 404);
 
+            // $ejemplares = Ejemplar::select(
+            //                                 'ejemplares.id',
+            //                                 'ejemplares.padre_id',
+            //                                 'ejemplares.madre_id',
+            //                                 'ejemplares.madre_id',
+            //                                 'ejemplares.fenotipo_id',
+            //                                 'ejemplares.color_id',
+            //                                 'ejemplares.nombre',
+            //                                 'ejemplares.sexo',
+            //                                 'ejemplares.fecha_nacimiento',
+            //                                 'ejemplares.numero_registro',
+            //                                 'ejemplares.microchip',
+            //                                 'ejemplares.arete',
+            //                                 'ejemplares.tipo'
+            //                                 )
+            //                         ->join('criaderos', 'ejemplares.criadero_id', '=', 'criaderos.id')
+            //                         ->where('criaderos.propietario_id', $usuario->id)->get();
+
             $ejemplares = Ejemplar::select(
-                                            'ejemplares.id',
-                                            'ejemplares.padre_id',
-                                            'ejemplares.madre_id',
-                                            'ejemplares.madre_id',
-                                            'ejemplares.fenotipo_id',
-                                            'ejemplares.color_id',
-                                            'ejemplares.nombre',
-                                            'ejemplares.sexo',
-                                            'ejemplares.fecha_nacimiento',
-                                            'ejemplares.numero_registro',
-                                            'ejemplares.microchip',
-                                            'ejemplares.arete',
-                                            'ejemplares.tipo'
-                                            )
-                                    ->join('criaderos', 'ejemplares.criadero_id', '=', 'criaderos.id')
-                                    ->where('criaderos.propietario_id', $usuario->id)->get();
+                'ejemplares.id',
+                'ejemplares.padre_id',
+                'ejemplares.madre_id',
+                'ejemplares.fenotipo_id',
+                'ejemplares.color_id',
+                'ejemplares.nombre',
+                'ejemplares.sexo',
+                'ejemplares.fecha_nacimiento',
+                'ejemplares.numero_registro',
+                'ejemplares.microchip',
+                'ejemplares.arete',
+                'ejemplares.tipo'
+            )
+            ->join('criaderos', 'ejemplares.criadero_id', '=', 'criaderos.id')
+            ->leftJoin('ejemplar_imagenes', 'ejemplares.id', '=', 'ejemplar_imagenes.ejemplar_id') // Unir las imágenes
+            ->where('criaderos.propietario_id', $usuario->id)
+            ->get()
+            ->map(function ($ejemplar) {
+                $ejemplar->imagenes = EjemplarImagen::where('ejemplar_id', $ejemplar->id)
+                    ->pluck('ruta');
+                return $ejemplar;
+            });
+
 
             return response()->json([
                 'usuario_id' => $usuario->id,
